2df8aa93db47c0c47be9291259402d5a1d795f79
Colin <colin.zimmermann@renuo.ch>
2023-02-23 10:10:34 +0100

    added input fields

--- App.js
+++ App.js
@@ -1,21 +1,53 @@
-import { StatusBar } from 'expo-status-bar';
+import {StyleSheet, Text, TextInput, View} from 'react-native';
 import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
 
-export default function App() {
-  return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
-  );
+function App() {
+    return (
+        <View style={{
+            flex: 1,
+            justifyContent: 'center',
+            backgroundColor: '#ecf01'
+        }}>
+            <InputWithLabel
+                label="Email"
+                placeholder="Type your email here"
+                value=""
+                onChangeText={() => {
+                }}
+            />
+            <InputWithLabel
+                label="Password"
+                placeholder="Type your password here"
+                value=""
+                onChangeText={() => {
+                }}
+            />
+            <InputWithLabel
+                label="Name"
+                placeholder="Type your pet's name here" 
+                value=""
+                onChangeText={() => {
+                }}
+            />
+            
+        </View>
+
+    )
 }
 
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-});
+function InputWithLabel(props) {
+    const {label, placeholder, value, onChangeText} = props;
+
+    return (
+        <View>
+            <Text>{label}</Text>
+            <TextInput
+                placeholder={placeholder}
+                value={value}
+                onChangeText={onChangeText}
+            />
+        </View>
+    );
+}
+
+export default App;
