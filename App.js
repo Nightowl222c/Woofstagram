fde6ec4ad9e0ac3eef5640dc97156389eab0bc0e
Colin <colin.zimmermann@renuo.ch>
2023-02-23 10:58:29 +0100

    added form styling

--- App.js
+++ App.js
@@ -78,14 +78,15 @@ function InputWithLabel(props) {
     const {label, placeholder, value, onChangeText, secureTextEntry, onSubmitEditing} = props;
 
     return (
-        <View>
-            <Text>{label}</Text>
+        <View style={{padding: 16}}>
+            <Text style={{padding: 8, fontSize: 18}}>{label}</Text>
             <TextInput
                 placeholder={placeholder}
                 value={value}
                 onChangeText={onChangeText}
                 secureTextEntry={secureTextEntry}
                 onSubmitEditing={onSubmitEditing}
+                style={{padding: 8, fontSize: 18}}
             />
         </View>
     );
